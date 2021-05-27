d2061ade957f393184bb95fbaef7fe6977f5d59d
pstaal <peter.staal@gmail.com>
2021-05-27 15:41:38 +0200

    Add login-view logic

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -10,7 +11,8 @@ class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     };
   }
 
@@ -32,9 +34,19 @@ class MainView extends React.Component {
     });
   }
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie, user } = this.state;
+    
+     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (movies.length === 0) return <div className="main-view" />;
 
