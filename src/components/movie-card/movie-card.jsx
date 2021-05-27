8b9d869ab77223b7be2bc62c2ee492f64967cbdb
pstaal <peter.staal@gmail.com>
2021-05-27 15:59:23 +0200

    Add proptypes to every component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,7 +11,20 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.number.isRequired,
+      Death: PropTypes.number
+    }),
+    Actors: PropTypes.array
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
